3930d6b53b8305b49a27271bc70738244b2ade7a
Mark Madsen <mark@agileanimal.com>
2011-11-06 23:54:46 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProLitConference::Application.config.secret_token = '899de280f2adee7ebcc1b153f149c526109641d12786e934740aa19954a2e414dc7835a492a29f8defcde94bc6b7667363cae326664583502e136055fadd648d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ProLitConference::Application.config.session_store :cookie_store, key: '_pro_lit_conference_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ProLitConference::Application.config.session_store :active_record_store
