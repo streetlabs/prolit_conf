6dbdfdc4f87ea303ee555e94a571541c66106cff
Hemant Kumar <gethemant@gmail.com>
2012-06-06 00:57:23 +0530

    Upgrade everything to rails 3.2.5

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ProLitConference::Application.config.secret_token = '899de280f2adee7ebcc1b153f149c526109641d12786e934740aa19954a2e414dc7835a492a29f8defcde94bc6b7667363cae326664583502e136055fadd648d'
+ProLitConference::Application.config.secret_token = '4b1cb8fc3fae44020e3b95ce2d0988ae30c09ec09537a4d621ab76e4a176ffe6c2ce15ab27993e5e321439dafe88bc9a3700db82a7c06fb6de7d67025f28f467'
