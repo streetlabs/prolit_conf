f6fbb1cec385b5857a8a3de41299ad6471637496
Yuva <yuva@codemancers.com>
2012-06-14 15:29:03 +0530

    put admin approvable logic from devise wiki

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,23 @@ class User < ActiveRecord::Base
   belongs_to :attendee
   validates :attendee_id, :email, :encrypted_password, :presence => true
   validates :email, :uniqueness => true
+
+  # https://github.com/plataformatec/devise/wiki/How-To:-Require-admin-to-activate-account-before-sign_in
+  def activate_for_authentication?
+    super && approved?
+  end
+
+  def inactive_message
+    approved? ? super() : :not_approved
+  end
+
+  def self.send_reset_password_instructions(attributes={})
+    recoverable = find_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+    if !recoverable.approved?
+      recoverable.errors[:base] << I18n.t("devise.failure.not_approved")
+    elsif recoverable.persisted?
+      recoverable.send_reset_password_instructions
+    end
+    recoverable
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120613094203) do
+ActiveRecord::Schema.define(:version => 20120614091721) do
 
   create_table "attendees", :force => true do |t|
     t.string   "name"
@@ -50,10 +50,10 @@ ActiveRecord::Schema.define(:version => 20120613094203) do
 
   create_table "users", :force => true do |t|
     t.integer  "attendee_id"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20120613094203) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.boolean  "approved",               :default => false
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
