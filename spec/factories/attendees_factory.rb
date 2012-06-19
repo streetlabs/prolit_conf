dd8141997f9719acfcdd7011084d4b3cf3c3544c
Yuva <yuva@codemancers.com>
2012-06-19 11:49:22 +0530

    rewrite attendees factory to consider inheritance

--- spec/factories/attendees_factory.rb
+++ spec/factories/attendees_factory.rb
@@ -1,26 +1,34 @@
 FactoryGirl.define do
+  sequence :attendee_name do |counter|
+    "Harry#{counter}"
+  end
+
   factory :attendee do
+    name   { generate(:attendee_name) }
     roles  Attendee::ROLES[:attendee]
-  end
 
-  factory :unconfirmed, :class => Attendee do
-    roles  Attendee::ROLES[:unconfirmed]
-  end
+    factory :unconfirmed do
+      roles  Attendee::ROLES[:unconfirmed]
+    end
 
-  factory :presenter, :class => Attendee do
-    roles  Attendee::ROLES[:presenter]
-  end
+    factory :presenter do
+      roles  Attendee::ROLES[:presenter]
+    end
 
-  factory :keynote, :class => Attendee do
-    roles  Attendee::ROLES[:keynote]
-  end
+    factory :keynote do
+      roles  Attendee::ROLES[:keynote]
+    end
 
-  factory :chair, :class => Attendee do
-    roles  Attendee::ROLES[:chair]
-  end
+    factory :chair do
+      roles  Attendee::ROLES[:chair]
+    end
 
-  factory :admin, :class => Attendee do
-    roles  Attendee::ROLES[:admin]
+    factory :admin do
+      roles  Attendee::ROLES[:admin]
+    end
+
+    after(:create) do |attendee|
+      attendee.user ||= FactoryGirl.create(:user, :attendee => attendee, :email => attendee.name + "@example.com")
+    end
   end
 end
-
