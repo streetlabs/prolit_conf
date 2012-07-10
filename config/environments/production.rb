302ae87d1b08080355942157894c3c2a04284a80
Mark Madsen <mark@agileanimal.com>
2012-07-10 09:09:30 -0600

    Updated name for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ ProLitConference::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => "plcs.anml.io" }
+  config.action_mailer.default_url_options = { :host => "http://conference.proletarianarts.org" }
 
   # Enable threaded mode
   # config.threadsafe!
